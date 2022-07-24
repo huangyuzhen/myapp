a6aff1dd04e3c101312c51862c778264dfe020f4
Huang Yuzhen <huangyuzhen@xiaomi.com>
2022-07-24 21:32:17 +0800

    server run

--- src/main/java/mycompany/server/ServerMain.java
+++ src/main/java/mycompany/server/ServerMain.java
@@ -1,5 +1,7 @@
 package mycompany.server;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.thrift.TProcessor;
 import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.server.TServer;
@@ -10,33 +12,26 @@ import org.apache.thrift.transport.layered.TFramedTransport;
 
 import thrift.service.ComputeServer;
 
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.LogManager;
-
 public class ServerMain {
     private static final Logger logger = LogManager.getLogger(ServerMain.class);
 
     public static void main(String[] args) {
         try {
-            //实现服务处理接口impl
             ThriftTestImpl workImpl = new ThriftTestImpl();
-            //创建TProcessor
-            TProcessor tProcessor = new ComputeServer.Processor<ComputeServer.Iface>(workImpl);
-            //创建TServerTransport,非阻塞式 I/O，服务端和客户端需要指定 TFramedTransport 数据传输的方式
-            final TNonblockingServerTransport transport = new TNonblockingServerSocket(9999);
-            //创建TProtocol
-            TThreadedSelectorServer.Args ttpsArgs = new TThreadedSelectorServer.Args(transport);
-            ttpsArgs.transportFactory(new TFramedTransport.Factory());
-            //二进制格式反序列化
-            ttpsArgs.protocolFactory(new TBinaryProtocol.Factory());
-            ttpsArgs.processor(tProcessor);
-            ttpsArgs.selectorThreads(16);
-            ttpsArgs.workerThreads(32);
-            logger.info("compute service server on port :" + 9999);
-            //创建TServer
-            TServer server = new TThreadedSelectorServer(ttpsArgs);
-            //启动Server
+            TProcessor processor = new ComputeServer.Processor<ComputeServer.Iface>(workImpl);
+      
+            TNonblockingServerTransport transport = new TNonblockingServerSocket(9999);
+      
+            TThreadedSelectorServer.Args servArgs = new TThreadedSelectorServer.Args(transport);
+            servArgs.processor(processor);
+            servArgs.transportFactory(new TFramedTransport.Factory());
+            servArgs.protocolFactory(new TBinaryProtocol.Factory());
+      
+            TServer server = new TThreadedSelectorServer(servArgs);
+            System.out.println("Running TThreadedSelectorServer Server");
+      
             server.serve();
+
         } catch (Exception e) {
             logger.error(e);
         }
